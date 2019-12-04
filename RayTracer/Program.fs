8e1a02f2f81860462ebe2dc6a9e4d98080320315
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-04 11:30:04 +0100

    Adds toy program

--- RayTracer/Program.fs
+++ RayTracer/Program.fs
@@ -1,6 +1,23 @@
 ﻿module Main
 
+open Tuple
+
+type Projectile = Tuple * Tuple
+type Environment = Tuple * Tuple
+
+let tick (e: Environment) (p: Projectile) =
+  let (gravity, wind) = e
+  let (position, velocity) = p
+  let newPos = add position velocity
+  let newVel = velocity |> add gravity |> add wind
+  printfn "Position: %A, Velocity: %A" newPos newVel
+  Projectile (newPos, newVel)
+
 [<EntryPoint>]
 let main argv =
-  printfn "%s" "Hey"
+  let projectile = (point 0.0 0.0 0.0), (vector 50.0 50.0 0.0)
+  let env = (vector 0.0 -9.8 0.0), (vector -1.0 0.0 0.0)
+
+  let ticks = [1..20] |> List.map (fun _ -> tick env) |> List.reduce (>>)
+  ticks projectile |> ignore
   0
