50ca7a686f8c9b2a9bda7abbf8b4b59800c61477
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-04 15:25:01 +0100

    Adds canvas read and write

--- RayTracer.Tests/Canvas.fs
+++ RayTracer.Tests/Canvas.fs
@@ -4,16 +4,23 @@ open Expecto
 open Canvas
 open Color
 
+let black = color 0.0 0.0 0.0
+let red = color 1.0 0.0 0.0
 let collapse l = List.reduce (List.append) l
-let isBlack = equals (color 0.0 0.0 0.0)
+let isBlack = equals black
 
 [<Tests>]
 
 let tests =
   testList "Tests for Canvas" [
     testCase "Creating a canvas" <| fun _ ->
-     let c = canvas 10 20
-     Expect.equal (width c) 10 ""
-     Expect.equal (height c) 20 ""
-     Expect.isTrue (collapse c |> List.forall isBlack) ""
+      let c = canvas 10 20
+      Expect.equal (width c) 10 ""
+      Expect.equal (height c) 20 ""
+      Expect.isTrue (collapse c |> List.forall isBlack) ""
+
+    testCase "Writing pixels to a canvas" <| fun _ ->
+      let c1 = canvas 10 20
+      let c2 = write c1 2 3 red
+      Expect.equal (read c2 2 3) (Some red) ""
   ]
--- RayTracer/Canvas.fs
+++ RayTracer/Canvas.fs
@@ -12,3 +12,29 @@ let width (c: Canvas) =
   | _ -> 0
 
 let height (c: Canvas) = List.length c
+
+let isIndex a (b, _) = a = b
+
+let get list index =
+  List.indexed list |> List.tryFind (isIndex index)
+
+let replace index (newEl: 'a) (list: 'a list)=
+  list |> List.mapi (fun i el ->
+    if (i = index) then newEl else el
+  )
+
+let write (canvas: Canvas) x y (color: Color) =
+  match (get canvas y) with
+  | Some (rowI, row) ->
+    match (get row x) with
+    | Some (colI, col) -> replace rowI (replace colI color row) canvas
+    | None -> canvas
+  | None -> canvas
+
+let read (canvas: Canvas) x y =
+  match (get canvas y) with
+  | Some (rowI, row) ->
+    match (get row x) with
+    | Some (colI, col) -> Some col
+    | None -> None
+  | None -> None
