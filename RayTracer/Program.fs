0df9c52e4f2da4456b75afd0e25f102d9491341b
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-04 20:43:45 +0100

    Adds toy simulation

--- RayTracer/Program.fs
+++ RayTracer/Program.fs
@@ -1,5 +1,6 @@
 ﻿module Main
 
+open System
 open Tuple
 open Canvas
 open Util
@@ -12,17 +13,34 @@ let tick (e: Environment) (p: Projectile) =
   let (position, velocity) = p
   let newPos = add position velocity
   let newVel = velocity |> add gravity |> add wind
-  printfn "Position: %A, Velocity: %A" newPos newVel
   Projectile (newPos, newVel)
+
+let toInt (a: float) = Math.Round a |> int
 
 [<EntryPoint>]
 let main argv =
-  let projectile = (point 0.0 0.0 0.0), (vector 50.0 50.0 0.0)
-  let env = (vector 0.0 -9.8 0.0), (vector -1.0 0.0 0.0)
+  let projectile = (point 0.0 100.0 0.0), (vector 15.0 -10.0 0.0)
+  let env = (vector 0.0 0.98 0.0), (vector -1.0 0.0 0.0)
 
-  let imageFile = toPpm (canvas 200 100)
+  let ticks =
+    [1..20]
+    |> List.fold (fun a _ -> (tick env (List.head a)) :: a) [projectile]
+    |> List.rev
+    |> List.map fst
+
+  let pixels =
+    ticks
+    |> List.filter (fun (x, y, _, _) -> y <= 100.0 && y >= 0.0 && x >= 0.0)
+    |> List.map (Tuple.map toInt >> fun (x, y, _, _) -> (x, y))
+
+  printfn "Pixels: %A" pixels
+
+  let white = Color.color 1.0 1.0 1.0
+  let c =
+    pixels
+    |> List.fold (fun acc (x, y) -> Canvas.write x y white acc) (canvas 200 100)
+
+  let imageFile = toPpm c
   writeFile @"./out.ppm" imageFile
 
-  let ticks = [1..20] |> List.map (fun _ -> tick env) |> List.reduce (>>)
-  ticks projectile |> ignore
   0
