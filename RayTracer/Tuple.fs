319253eb9a5b266fd7e857c20f2baa34a19c9452
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-04 10:44:47 +0100

    Renames tupleOp -> combine

--- RayTracer/Tuple.fs
+++ RayTracer/Tuple.fs
@@ -10,7 +10,7 @@ let vector x y z = Tuple (x, y, z, 0.0)
 let epsilon = 0.00001
 let equals a b = abs (a - b) < epsilon
 
-let tupleOp op (a: Tuple) (b: Tuple) =
+let comnine op (a: Tuple) (b: Tuple) =
   let (x1, y1, z1, w1) = a
   let (x2, y2, z2, w2) = b;
   Tuple (op x1 x2, op y1 y2, op z1 z2, op w1 w2)
@@ -20,12 +20,12 @@ let map fn t =
   Tuple (fn x, fn y, fn z, fn w)
 
 
-let add = tupleOp (+)
+let add = comnine (+)
 
-let subtract = tupleOp (-)
+let subtract = comnine (-)
 
 let zero = vector 0.0 0.0 0.0
-let negate = tupleOp (-) zero
+let negate = comnine (-) zero
 
 let multiply n = map ((*) n)
 
