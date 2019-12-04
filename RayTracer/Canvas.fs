88211e4245eb3fa1c1df7ea81266b3d445bf4c28
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-04 15:57:13 +0100

    Moves things to Util

--- RayTracer/Canvas.fs
+++ RayTracer/Canvas.fs
@@ -1,6 +1,7 @@
 module Canvas
 
 open Color
+open Util
 
 type Canvas = (Color list) list
 let canvas w h =
@@ -12,24 +13,6 @@ let width (c: Canvas) =
   | _ -> 0
 
 let height (c: Canvas) = List.length c
-
-let isIndex a (b, _) = a = b
-
-let get list index =
-  List.indexed list |> List.tryFind (isIndex index)
-
-let replace index (newEl: 'a) (list: 'a list)=
-  list |> List.mapi (fun i el ->
-    if (i = index) then newEl else el
-  )
-
-let get2d x y (list: 'a list list) =
-  match (get list y) with
-  | Some (rowI, row) ->
-    match (get row x) with
-    | Some (colI, col) -> Some (row, col)
-    | None -> None
-  | None -> None
 
 let write x y (color: Color) (canvas: Canvas) =
   match (get2d x y canvas) with
--- RayTracer/Util.fs
+++ RayTracer/Util.fs
@@ -3,3 +3,21 @@ module Util
 let pow a b = b ** a
 
 let flip fn a b = fn b a
+
+
+let isIndex a b = a = fst b
+let get list index =
+  List.indexed list |> List.tryFind (isIndex index)
+
+let replace index (newEl: 'a) (list: 'a list)=
+  list |> List.mapi (fun i el ->
+    if (i = index) then newEl else el
+  )
+
+let get2d x y (list: 'a list list) =
+  match (get list y) with
+  | Some (rowI, row) ->
+    match (get row x) with
+    | Some (colI, col) -> Some (row, col)
+    | None -> None
+  | None -> None
