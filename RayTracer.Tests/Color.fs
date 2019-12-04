2cfcce3b6f95eec02e9428c7d23045e4e0de3fb5
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-04 13:43:55 +0100

    Adds Color

---
+++ RayTracer.Tests/Color.fs
@@ -0,0 +1,28 @@
+module ColorTest
+
+open Expecto
+open Color
+
+[<Tests>]
+
+let tests =
+  testList "Tests for Unit" [
+    testCase "Colors are (red, green, blue) tuples" <| fun _ ->
+      let c = color -0.5 0.4 1.7
+      Expect.equal (c) (-0.5, 0.4, 1.7) ""
+
+    testCase "Adding colors" <| fun _ ->
+      let c1 = color 0.9 0.6 0.75
+      let c2 = color 0.7 0.1 0.25
+      Expect.equal (add c1 c2) (color 1.6 0.7 1.0) ""
+
+    testCase "Subtracting colors" <| fun _ ->
+      let c1 = color 0.9 0.6 0.75
+      let c2 = color 0.7 0.1 0.25
+      let c3 = subtract c1 c2
+      Expect.equal true (equals c3 (color 0.2 0.5 0.5)) ""
+
+    testCase "Multiplying a color by a scalar" <| fun _ ->
+      let c = color 0.2 0.3 0.4
+      Expect.equal (multiply 2.0 c) (color 0.4 0.6 0.8) ""
+  ]
---
+++ RayTracer/Color.fs
@@ -0,0 +1,34 @@
+module Color
+
+open Util
+
+type Color = float * float * float
+
+let color r g b = (r, g, b)
+
+let combine op (a: Color) (b: Color) =
+  let (x1, y1, z1) = a
+  let (x2, y2, z2) = b
+  (op x1 x2, op y1 y2, op z1 z2)
+
+let map fn t =
+  let (x, y, z) = t
+  Color (fn x, fn y, fn z)
+
+let epsilon = 0.00001
+let valueEquals a b = abs (a - b) < epsilon
+
+let equals a b = (combine valueEquals a b) |> function
+  | (true, true, true) -> true
+  | _ -> false
+
+
+type Add = Color -> Color -> Color
+let (add: Add) = combine (+)
+
+type Subtract = Color -> Color -> Color
+let (subtract: Subtract) = combine (-)
+
+let multiply n = map ((*) n)
+
+let divide n = map (flip (/) n)
