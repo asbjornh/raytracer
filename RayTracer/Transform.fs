d8fc2204253428b7fc42f782497a9913c8f6dd83
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-06 22:27:38 +0100

    Adds chainable transform functions

--- RayTracer/Transform.fs
+++ RayTracer/Transform.fs
@@ -29,3 +29,11 @@ let shearing xy xz yx yz zx zy =
   |> replace 0 1 xy |> replace 0 2 xz
   |> replace 1 0 yx |> replace 1 2 yz
   |> replace 2 0 zx |> replace 2 1 zy
+
+let translate x y z = translation x y z |> multiply
+let scale x y z = scaling x y z |> multiply
+let rotateX rad = rotationX rad |> multiply
+let rotateY rad = rotationY rad |> multiply
+let rotateZ rad = rotationZ rad |> multiply
+let shear xy xz yx yz zx zy =
+  shearing xy xz yx yz zx zy |> multiply
