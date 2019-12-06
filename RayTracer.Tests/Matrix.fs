45efc1cec0d924426cc2ace00d19c9745dd009b5
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-06 20:06:19 +0100

    Adds scaling

--- RayTracer.Tests/Matrix.fs
+++ RayTracer.Tests/Matrix.fs
@@ -304,4 +304,24 @@ let tests =
       let transform = translation 5. -3. 2.
       let v = Tuple.vector -3. 4. 5.
       Expect.equal (multiplyTuple transform v) v ""
+
+    testCase "A scaling matrix applied to a point" <| fun _ ->
+      let transform = scaling 2. 3. 4.
+      let p = Tuple.point -4. 6. 8.
+      Expect.equal (multiplyTuple transform p) (Tuple.point -8. 18. 32.) ""
+
+    testCase "A scaling matrix applied to a vector" <| fun _ ->
+      let transform = scaling 2. 3. 4.
+      let v = Tuple.vector -4. 6. 8.
+      Expect.equal (multiplyTuple transform v) (Tuple.vector -8. 18. 32.) ""
+
+    testCase "Multiplying by the inverse of a scaling matrix" <| fun _ ->
+      let transform = scaling 2. 3. 4.
+      let v = Tuple.vector -4. 6. 8.
+      Expect.equal (multiplyTuple (inverse transform) v) (Tuple.vector -2. 2. 2.) ""
+
+    testCase "Reflection is scaling by a negative value" <| fun _ ->
+      let transform = scaling -1. 1. 1.
+      let p = Tuple.point 2. 3. 4.
+      Expect.equal (multiplyTuple transform p) (Tuple.point -2. 3. 4.) ""
   ]
--- RayTracer/Matrix.fs
+++ RayTracer/Matrix.fs
@@ -90,3 +90,6 @@ let inverse a =
 
 let translation x y z =
   identity () |> replace 0 3 x |> replace 1 3 y |> replace 2 3 z
+
+let scaling x y z =
+  identity () |> replace 0 0 x |> replace 1 1 y |> replace 2 2 z
