27f47c4b13d2a51ee82795f735b7ad195d441039
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-08 19:20:44 +0100

    Adds test case for Sphere

--- RayTracer.Tests/Sphere.fs
+++ RayTracer.Tests/Sphere.fs
@@ -46,4 +46,12 @@ let tests =
       Expect.equal (List.length xs) 2 ""
       Expect.equal xs.[0].t -6. ""
       Expect.equal xs.[1].t -4. ""
+
+    testCase "Intersect sets the object on the intersection" <| fun _ ->
+      let r = ray (point 0. 0. -5.) (vector 0. 0. 1.)
+      let s = sphere ()
+      let xs = intersect s r
+      Expect.equal (List.length xs) 2 ""
+      Expect.equal (xs.[0].object) s ""
+      Expect.equal (xs.[1].object) s ""
   ]
