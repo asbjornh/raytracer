db818960a3f2449e6a1a47cedc80ec08683f48b7
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-08 13:20:24 +0100

    Moves intersect function to Sphere

--- RayTracer/Ray.fs
+++ RayTracer/Ray.fs
@@ -13,16 +13,3 @@ let ray origin direction =
 
 let position t ray =
   ray.origin + t * ray.direction
-
-let intersect s ray =
-  let sphereToRay = ray.origin - (point 0. 0. 0.)
-  let a = dot ray.direction ray.direction
-  let b = 2. * (dot ray.direction sphereToRay)
-  let c = (dot sphereToRay sphereToRay) - 1.
-  let discriminant = pow 2. b - 4. * a * c
-  if (discriminant < 0.)
-  then []
-  else
-    let t1 = (-b - sqrt discriminant) / (2. * a)
-    let t2 = (-b + sqrt discriminant) / (2. * a)
-    [t1; t2]
--- RayTracer/Sphere.fs
+++ RayTracer/Sphere.fs
@@ -1,3 +1,20 @@
 module Sphere
 
+open Ray
+open Tuple
+open Util
+
 let sphere () = ()
+
+let intersect s ray =
+  let sphereToRay = ray.origin - (point 0. 0. 0.)
+  let a = dot ray.direction ray.direction
+  let b = 2. * (dot ray.direction sphereToRay)
+  let c = (dot sphereToRay sphereToRay) - 1.
+  let discriminant = pow 2. b - 4. * a * c
+  if (discriminant < 0.)
+  then []
+  else
+    let t1 = (-b - sqrt discriminant) / (2. * a)
+    let t2 = (-b + sqrt discriminant) / (2. * a)
+    [t1; t2]
