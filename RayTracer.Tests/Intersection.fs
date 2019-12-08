3b09073f6fcac865c1cf5484cc970c2a81521cd1
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-08 19:46:10 +0100

    Adds intersections and hit in Intersection module

--- RayTracer.Tests/Intersection.fs
+++ RayTracer.Tests/Intersection.fs
@@ -12,4 +12,34 @@ let tests =
       let i = intersection 3.5 s
       Expect.equal i.t 3.5 ""
       Expect.equal i.object s ""
+
+    testCase "The hit, when all intersections have positive t" <| fun _ ->
+      let s = sphere ()
+      let i1 = intersection 1. s
+      let i2 = intersection 2. s
+      let xs = intersections [i2; i1]
+      Expect.equal (hit xs) (Some i1) ""
+
+    testCase "The hit, when some intersections have negative t" <| fun _ ->
+      let s = sphere ()
+      let i1 = intersection -1. s
+      let i2 = intersection 1. s
+      let xs = intersections [i2; i1]
+      Expect.equal (hit xs) (Some i2) ""
+
+    testCase "The hit, when all intersections have negative t" <| fun _ ->
+      let s = sphere ()
+      let i1 = intersection -2. s
+      let i2 = intersection -1. s
+      let xs = intersections [i2; i1]
+      Expect.equal (hit xs) None ""
+
+    testCase "The hit is always the lowest nonnegative intersection" <| fun _ ->
+      let s = sphere ()
+      let i1 = intersection 5. s
+      let i2 = intersection 7. s
+      let i3 = intersection -3. s
+      let i4 = intersection 2. s
+      let xs = intersections [i1; i2; i3; i4]
+      Expect.equal (hit xs) (Some i4) ""
   ]
--- RayTracer/Intersection.fs
+++ RayTracer/Intersection.fs
@@ -2,4 +2,16 @@ module Intersection
 
 type Intersection<'a> = { t: float; object: 'a }
 
+let getT i = i.t
+
 let intersection t o = { t=t; object=o }
+
+let intersections (l: Intersection<'a> list) =
+  List.sortBy getT l
+
+let visible i = i.t >= 0.
+let hit (l: Intersection<'a> list) =
+  let visibleEls = List.filter visible l
+  match visibleEls with
+  | [] -> None
+  | _ -> Some (List.minBy getT visibleEls)
