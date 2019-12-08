840c33f6c84cbe99aaa6390e34c37ff14c9f0531
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-09 00:39:55 +0100

    Refactors Color

--- RayTracer/Color.fs
+++ RayTracer/Color.fs
@@ -4,15 +4,15 @@ open Util
 
 type Color = float * float * float
 
-let color r g b = (r, g, b)
+let color r g b : Color = (r, g, b)
 
 let combine op (a: Color) (b: Color) =
   let (x1, y1, z1) = a
   let (x2, y2, z2) = b
   (op x1 x2, op y1 y2, op z1 z2)
 
-let map fn t =
-  let (x, y, z) = t
+let map fn (c: Color) =
+  let (x, y, z) = c
   (fn x, fn y, fn z)
 
 let toList (r, g, b) = [r; g; b]
@@ -29,21 +29,11 @@ let equals a b = (combine valueEquals a b) |> function
   | (true, true, true) -> true
   | _ -> false
 
-
-type Add = Color -> Color -> Color
-let (add: Add) = combine (+)
-
-type Subtract = Color -> Color -> Color
-let (subtract: Subtract) = combine (-)
-
-type Multiply = Color -> Color -> Color
-let (multiply: Multiply) = combine (*)
-
-type Scale = float -> Color -> Color
-let (scale: Scale) = fun n -> map ((*) n)
-
-type Divide = float -> Color -> Color
-let (divide: Divide) = fun n -> map (flip (/) n)
+let add = combine (+)
+let subtract = combine (-)
+let multiply = combine (*)
+let scale n = map ((*) n)
+let divide n = map (flip (/) n)
 
 let red = color 1. 0. 0.
 let green = color 0. 1. 0.
