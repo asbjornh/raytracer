cafad95b9385be957fbb1622615e94352c838367
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-13 13:21:18 +0100

    Tweaks Stripes demo

--- RayTracer/Demos/Stripes.fs
+++ RayTracer/Demos/Stripes.fs
@@ -11,52 +11,57 @@ open Pattern
 open Shape
 open Transform
 open Tuple
+open Util
 open World
 
 
-let mat = materialC (color 0.5 0.1 0.6)
+let mat = material yellow 0.3 0.8 0.8
 
 let floor = { defaultPlane () with Material = mat }
 
+let wall = plane (chain [translateZ 10.; rotateX (rad 80.)]) mat
 
-let midPattern = stripePatternT darkGray white (chain [
+
+
+let purple = color 0. 0. 0.2
+let midPattern = stripePatternT purple white (chain [
+  translateX -0.06
   rotateY (Math.PI / 1.5)
   uniformScale 0.05
 ])
 let middle =
   sphere
   <| (translation -0.5 1. 0.5)
-  <| patternMaterial midPattern 0.1 0.9 0.9
+  <| patternMaterial midPattern 0.25 0.85 0.85
 
-let rightPattern = stripePatternT darkGray white (chain [
+let rightPattern = stripePatternT purple white (chain [
   rotateZ (Math.PI / 2.)
   uniformScale 0.3
 ])
-
 let right =
   sphere
   <| (chain [ translate 1.5 0.5 -0.5; uniformScale 0.5 ])
-  <| patternMaterial rightPattern 0.1 0.9 0.9
+  <| patternMaterial rightPattern 0.25 0.85 0.85
 
-let leftPattern = stripePatternT darkGray white (identity ())
+let leftPattern = stripePatternT purple white (identity ())
 let left =
   sphere
   <| (chain [ translate -1.5 0.33 -0.75; uniformScale 0.33 ])
-  <| patternMaterial leftPattern 0.1 0.9 0.9
+  <| patternMaterial leftPattern 0.25 0.85 0.85
 
-let darkBlue = color 0. 0.1 0.2
+let darkBrown = Color.scale 0.15 (color 1. 0.3 0.6)
 let pLight = pointLight (point -10. 10. -10.) (color 1. 0.9 0.7)
-let cLight = constantLight darkBlue
+let cLight = constantLight darkBrown
 let cam = camera 400 200 (Math.PI / 3.)
 let cTransform = viewTransform (point 0. 1.5 -5.) (point 0. 1. 0.) (vector 0. 1. 0.)
 cam.transform <- cTransform
 
 let objects: IShape list =
-  [middle; right; left; floor]
+  [middle; right; left; floor; wall]
 
 let w = {
   world [pLight; cLight] objects with
-    background = darkBlue
+    background = darkBrown
 }
 
 let run () =
