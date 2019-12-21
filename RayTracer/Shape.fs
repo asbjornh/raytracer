621923dddd3d885da7251f8f6fd22ce144c57e1f
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-21 17:03:09 +0100

    Refactors shape factories

--- RayTracer/Shape.fs
+++ RayTracer/Shape.fs
@@ -134,22 +134,20 @@ let normalAt point (shape: Shape) =
   let (x, y, z, _) = worldN.Return
   normalize (vector x y z)
 
-let sphere t m = {
-  transform = t
-  material = m
-  shape = Sphere
-}
-let unitSphere () = sphere (identity ()) (defaultMaterial ())
-let sphereT t = sphere t (defaultMaterial ())
-let sphereM m = sphere (identity ()) m
+let shape s t m = { transform = t; material = m; shape = s }
+let shapeM s m = shape s (identity ()) m
+let shapeT s t = shape s t <| defaultMaterial ()
+let defaultShape s = shape s <| identity () <| defaultMaterial ()
+let sphere t = shape Sphere t
+let plane t = shape Plane t
+let cube t = shape Cube t
+let cylinder t = shape Cylinder t
+let openCylinder t = shape OpenCylinder t
 
-let plane t m = { transform = t; material = m; shape = Plane }
-let defaultPlane () = plane (identity ()) (defaultMaterial ())
-
-let cube t m = { transform = t; material = m; shape = Cube }
-let unitCube () = cube <| identity () <| defaultMaterial ()
-
-let cylinder t m = { transform = t; material = m; shape = Cylinder }
-let defaultCylinder () = cylinder <| identity () <| defaultMaterial ()
-let openCylinder t m = { transform = t; material = m; shape = OpenCylinder }
-let defaultOpenCylinder () = openCylinder <| identity () <| defaultMaterial ()
+let unitSphere () = defaultShape Sphere
+let sphereT t = shapeT Sphere t
+let sphereM m = shapeM Sphere m
+let defaultPlane () = defaultShape Plane
+let unitCube () = defaultShape Cube
+let defaultCylinder () = defaultShape Cylinder
+let defaultOpenCylinder () = defaultShape OpenCylinder
