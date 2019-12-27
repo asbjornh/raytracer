b157138c13245922ec880a75156376ba2cfed744
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-27 18:34:09 +0100

    Adds parsing of faces in ObjParser

--- RayTracer.Tests/ObjParser.fs
+++ RayTracer.Tests/ObjParser.fs
@@ -3,7 +3,12 @@ module ObjParserTest
 open Expecto
 
 open ObjParser
+open Shape
 open Tuple
+
+let getPoly s =
+  match s.shape with
+  | Poly p -> p | _ -> failwith "Bajs"
 
 [<Tests>]
 let tests =
@@ -31,4 +36,50 @@ let tests =
       Expect.equal r.vertices.[1] (point -1. 0.5 0.) ""
       Expect.equal r.vertices.[2] (point 1. 0. 0.) ""
       Expect.equal r.vertices.[3] (point 1. 1. 0.) ""
+
+    testCase "Parsing triangle faces" <| fun _ ->
+      let file = [
+        "v -1 1 0​"
+        "v -1 0 0​"
+        "v 1 0 0​"
+        "v 1 1 0​"
+        ""
+        "f 1 2 3​"
+        "f 1 3 4​"
+      ]
+      let r = parse file
+      let g = r.defaultGroup
+      let t1 = getPoly g.children.[0]
+      let t2 = getPoly g.children.[1]
+      Expect.equal t1.p1 r.vertices.[0] ""
+      Expect.equal t1.p2 r.vertices.[1] ""
+      Expect.equal t1.p3 r.vertices.[2] ""
+      Expect.equal t2.p1 r.vertices.[0] ""
+      Expect.equal t2.p2 r.vertices.[2] ""
+      Expect.equal t2.p3 r.vertices.[3] ""
+
+    testCase "Triangulating polygons" <| fun _ ->
+      let file = [
+        "v -1 1 0​"
+        "v -1 0 0​"
+        "v 1 0 0​"
+        "v 1 1 0​"
+        "v 0 2 0​"
+        ""
+        "f 1 2 3 4 5​"
+      ]
+      let r = parse file
+      let g = r.defaultGroup
+      let t1 = getPoly g.children.[0]
+      let t2 = getPoly g.children.[1]
+      let t3 = getPoly g.children.[2]
+      Expect.equal t1.p1 r.vertices.[0] ""
+      Expect.equal t1.p2 r.vertices.[1] ""
+      Expect.equal t1.p3 r.vertices.[2] ""
+      Expect.equal t2.p1 r.vertices.[0] ""
+      Expect.equal t2.p2 r.vertices.[2] ""
+      Expect.equal t2.p3 r.vertices.[3] ""
+      Expect.equal t3.p1 r.vertices.[0] ""
+      Expect.equal t3.p2 r.vertices.[3] ""
+      Expect.equal t3.p3 r.vertices.[4] ""
   ]
--- RayTracer/ObjParser.fs
+++ RayTracer/ObjParser.fs
@@ -2,11 +2,15 @@ module rec ObjParser
 
 open FParsec
 
+open Matrix
+open Shape
 open Tuple
+open Util
 
 type ParseResult = {
   ignoredLines: int
   vertices: Tuple list
+  defaultGroup: Shape
 }
 
 let exists = function | Some _ -> true | None -> false
@@ -14,21 +18,46 @@ let parse (txt: string list) =
   let vertices =
     List.map parseVertex txt
     |> List.collect Option.toList
+  let faces =
+    List.map parseFace txt
+    |> List.collect Option.toList
+    |> List.collect (List.map (int >> flip (-) 1) >> polys vertices)
+    |> groupT <| identity ()
 
   {
     ignoredLines = List.length txt - List.length vertices
     vertices = vertices
+    defaultGroup = faces
   }
+
+let polys vs face =
+  match face with
+  | [one; two; three] -> 
+    [defaultPoly vs.[one] vs.[two] vs.[three]]
+  | _ ->
+    face |> List.map (fun i -> vs.[i])
+    |> fanTriangulation
+
+let fanTriangulation = function
+  | [] -> []
+  | start :: rest ->
+    List.pairwise rest
+    |> List.map (fun (second, third) ->
+      defaultPoly start second third
+    )
 
 let str s = pstring s
 let coords = sepBy pfloat <| pstring " "
-let vertex =
-  pchar 'v' >>. str " " >>. coords
+let vertex = pchar 'v' >>. str " " >>. coords
+let face = pchar 'f' >>. str " " >>. coords
 
 let parseVertex s =
   match run vertex s with
+  | Success ([x; y; z], _, _) -> Some <| point x y z
+  | _ -> None
+
+let parseFace s =
+  match run face s with
   | Success (s, _, _) ->
-    match s with
-    | [x; y; z] -> Some <| point x y z
-    | _ -> None
+    if List.length s >= 3 then Some s else None
   | _ -> None
