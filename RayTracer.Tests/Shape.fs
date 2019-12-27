74d77ea92f1426915e2a6ff8f3ff165458a63157
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-12-27 15:09:23 +0100

    Moves tests for Plane into separate file

--- RayTracer.Tests/Shape.fs
+++ RayTracer.Tests/Shape.fs
@@ -50,45 +50,6 @@ let tests =
       let n = normalAt s (point 0. a -a)
       Expect.equal n (vector 0. 0.97014 -0.24254) ""
 
-    testCase "The normal of a plane is constant everywhere" <| fun _ ->
-      let p = defaultPlane ()
-      let n1 = localNormal p (point 0. 0. 0.)
-      let n2 = localNormal p (point 10. 0. -10.)
-      let n3 = localNormal p (point -5. 0. 150.)
-      Expect.equal n1 (vector 0. 1. 0.) ""
-      Expect.equal n2 (vector 0. 1. 0.) ""
-      Expect.equal n3 (vector 0. 1. 0.) ""
-
-    testCase "Intersect with a ray parallel to the plane" <| fun _ ->
-      let p = defaultPlane ()
-      let r = ray (point 0. 10. 0.) (vector 0. 0. 1.)
-      let xs = localIntersect r p
-      Expect.isEmpty xs ""
-
-    testCase "Intersect with a coplanar ray" <| fun _ ->
-      let p = defaultPlane ()
-      let r = ray (point 0. 0. 0.) (vector 0. 0. 1.)
-      let xs = localIntersect r p
-      Expect.isEmpty xs ""
-
-    testCase "A ray intersecting a plane from above" <| fun _ ->
-      let p = defaultPlane ()
-      let r = ray (point 0. 1. 0.) (vector 0. -1. 0.)
-      let xs = localIntersect r p
-      Expect.equal (List.length xs) 1 ""
-      let (t, object) = xs.[0]
-      Expect.equal t 1. ""
-      Expect.equal object p ""
-
-    testCase "A ray intersecting a plane from below" <| fun _ ->
-      let p = defaultPlane ()
-      let r = ray (point 0. -1. 0.) (vector 0. 1. 0.)
-      let xs = localIntersect r p
-      Expect.equal (List.length xs) 1 ""
-      let (t, object) = xs.[0]
-      Expect.equal t 1. ""
-      Expect.equal object p ""
-
     testCase "Cube intersection +x" <| fun _ ->
       testCubeIntersect (point 5. 0.5 0.) (vector -1. 0. 0.) 4. 6.
 
