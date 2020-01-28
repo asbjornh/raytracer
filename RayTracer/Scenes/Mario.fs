565ba43b00178368b4973d22237e0cb560692df6
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2020-01-28 19:13:25 +0100

    Groups objects

--- RayTracer/Scenes/Mario.fs
+++ RayTracer/Scenes/Mario.fs
@@ -5,6 +5,7 @@ open Color
 open Light
 open Material
 open Materials
+open Matrix
 open ObjParser
 open Shape
 open Transform
@@ -12,15 +13,9 @@ open Tuple
 open Util
 open World
 
-let t =
-  chain [
-    uniformScale 0.05f
-    rotateX (rad32 5.f)
-    rotateY (rad32 160.f)
-  ]
 let pigSkin = color 1. 0.7 0.58
 let face =
-  importObj "../models/mario/face.obj" t
+  importObj "../models/mario/face.obj" identity
   <| nintendo
       (color 0.5 0.26 0.27)
       (Color.scale 0.6 pigSkin)
@@ -28,7 +23,7 @@ let face =
       (add pigSkin white |> Color.scale 0.9)
 
 let hair =
-  importObj "../models/mario/hair.obj" t
+  importObj "../models/mario/hair.obj" identity
   <| nintendo
       (color 0.15 0.03 0.03)
       (color 0.3 0.16 0.1)
@@ -43,17 +38,61 @@ let facialHair =
     (Color.scale 3. white)
 
 let mustache =
-  importObj "../models/mario/mustache.obj" t facialHair
+  importObj "../models/mario/mustache.obj" identity facialHair
+
+let leftEyebrow =
+  importObj "../models/mario/eyebrow-left.obj" identity facialHair
+
+let rightEyebrow =
+  importObj "../models/mario/eyebrow-right.obj" identity facialHair
+
+let badge =
+  importObj "../models/mario/hat-badge.obj" identity
+  <| nintendo
+      (mix 0.5 blue (gray 0.5))
+      white black
+      (Color.scale 2. white)
+
+let hat =
+  importObj "../models/mario/hat.obj" identity
+  <| nintendo
+      (color 0.3 0. 0.0)
+      (color 0.6 0.1 0.2)
+      black
+      (Color.scale 1.7 white)
+
+let symbol =
+  importObj "../models/mario/m-symbol.obj" identity
+    <| material red 0.5 0.7 0.
+
+let faceGroup =
+  groupT
+    [
+      face
+      mustache
+      leftEyebrow
+      rightEyebrow
+      hair
+      hat
+      badge
+      symbol
+    ]
+    <| chain [
+      uniformScale 0.05f
+      rotateX (rad32 5.f)
+      rotateY (rad32 160.f)
+    ]
 
 let eyeBallMat uvTransform =
   Blend {
     mode = Multiply
     a = nintendo
-      (mix blue (gray 0.5) 0.5)
+      (mix 0.5 blue (gray 0.5))
       white black
       (Color.scale 2. white)
     b = luminanceTex "../models/mario/eye-color.png" uvTransform
   }
+
 let leftEye =
   sphere
   <| chain [
@@ -74,49 +113,17 @@ let rightEye =
   ]
   <| eyeBallMat (0.55, 0.45, 0.1, -0.57)
 
-let leftEyebrow =
-  importObj "../models/mario/eyebrow-left.obj" t facialHair
-
-let rightEyebrow =
-  importObj "../models/mario/eyebrow-right.obj" t facialHair
-
-let badge =
-  importObj "../models/mario/hat-badge.obj" t
-  <| nintendo
-      (mix blue (gray 0.5) 0.5)
-      white black
-      (Color.scale 2. white)
-
-let hat =
-  importObj "../models/mario/hat.obj" t
-  <| nintendo
-      (color 0.3 0. 0.0)
-      (color 0.6 0.1 0.2)
-      black
-      (Color.scale 1.7 white)
-
-let symbol =
-  importObj "../models/mario/m-symbol.obj" t
-    <| material red 0.5 0.7 0.
-
 let mario =
-  [ face
-    mustache
-    leftEyebrow
-    rightEyebrow
-    hair
-    hat
-    badge
-    symbol
-  ]
+  groupT [faceGroup; leftEye; rightEye]
+  <| chain [rotateX (rad32 5.f)]
 
 let sLight = pointLight (point -10. 10. -10.) (color 1. 1. 0.9)
 let cam =
-  camera 400 400 (rad32 60.f)
+  camera 200 200 (rad32 60.f)
   <| (point 0. 2. -4.8) <| (point 0. 2. 0.)
   
 let w = 
-  { world [sLight] ([leftEye; rightEye] @ mario) with
+  { world [sLight] [mario] with
       shadows = true
       background = gray 0.12
   }
