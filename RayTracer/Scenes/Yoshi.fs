832bd697d5d06184bc062c957d4c788dbd4ed178
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2020-02-05 00:11:50 +0100

    Updates Yoshi scene

--- RayTracer/Scenes/Yoshi.fs
+++ RayTracer/Scenes/Yoshi.fs
@@ -20,39 +20,57 @@ let backDrop =
     rotateX (rad32 90.f)
     uniformScale 10.f
   ]
-  <| luminanceTex "../tex/stars.jpg" (0.2, 0.15, 0.5, 0.5)
+  <| luminanceTex "../models/yoshi/background.jpg" (0.07, 0.05, 0.45, 0.5)
 
 // TODO: Figure out why Y scale is negative
-let tex =
-  NormalMap {
-    mat = Textured {
-      textureRaw "../models/yoshi/body-albedo.png" (1., -1., 0., 0.) with
-        ambient = 0.6
-        diffuse = 0.4
+let tex ambient diffuse =
+  Fresnel {
+    a = NormalMap {
+      mat = Textured {
+        textureRaw "../models/yoshi/body-albedo.png" (1., -1., 0., 0.) with
+          ambient = ambient
+          diffuse = diffuse
+      }
+      tex = Texture.read "../models/yoshi/body-normal.png"
+      uvTransform = (1., -1., 0., 0.)
     }
-    tex = Texture.read "../models/yoshi/body-normal.png"
-    uvTransform = (1., -1., 0., 0.)
+    blend = BlendingMode.SoftLight
+    b = Luminance (Color.scale 2.5 white)
+    power = 2.5
+    mixInner = 1.
+    mixOuter = 1.
   }
 
-let eyesTex =
-  Textured {
-    textureRaw "../models/yoshi/eye-albedo.png" (1., -1., 0., 0.) with
-      ambient = 0.6
-      diffuse = 0.4
+let eyesTex uOffset vOffset =
+  Fresnel {
+    a = Blend {
+      mode = Multiply
+      a = Textured {
+        textureRaw "../models/yoshi/eye-background.png" (1., -1., 0., 0.) with
+          ambient = 0.6
+          diffuse = 0.6
+      }
+      b = luminanceTex "../models/yoshi/eye-pupil.png" (1., -1., uOffset, vOffset)
+    }
+    blend = Add
+    b = Luminance (gray 0.6)
+    power = 3.
+    mixInner = 1.
+    mixOuter = 1.
   }
 
 let leftEye =
-  importObj "../models/yoshi/left-eye.obj" identity eyesTex
+  importObj "../models/yoshi/left-eye.obj" identity (eyesTex -0.25 -0.08)
 let rightEye =
-  importObj "../models/yoshi/right-eye.obj" identity eyesTex
+  importObj "../models/yoshi/right-eye.obj" identity (eyesTex 0.15 -0.02)
 let nose =
-  importObj "../models/yoshi/nose.obj" identity tex
+  importObj "../models/yoshi/nose.obj" identity (tex 0.9 0.5)
 let jaw =
-  importObj "../models/yoshi/jaw.obj" identity tex
+  importObj "../models/yoshi/jaw.obj" identity (tex 1. 0.3)
 let hair =
-  importObj "../models/yoshi/hair.obj" identity tex
+  importObj "../models/yoshi/hair.obj" identity (tex 0.9 0.5)
 let head =
-  importObj "../models/yoshi/head.obj" identity tex
+  importObj "../models/yoshi/head.obj" identity (tex 0.9 0.5)
 
 let yoshi =
   group [
@@ -63,23 +81,27 @@ let yoshi =
     head
     hair
   ]
-  <| rotateY (rad32 120.f)
+  <| chain [
+    rotateX (rad32 5.f)
+    rotateY (rad32 120.f)
+  ]
   <| defaultMaterial
 
-let light = pointLight (point -10. 10. -10.) (color 1. 1. 0.9)
+let light = pointLight (point -7. 10. -10.) (color 1. 1. 0.9)
 let cam =
-  camera 300 200 (rad32 35.f)
-  <| (point 0. 1. -8.) <| (point 0. 1. 0.)
+  camera 600 400 (rad32 20.f)
+  <| (point 0. 1. -14.) <| (point 0.25 0.9 0.)
   
 let w = 
-  { world [light] [yoshi] with
+  { world [light] [backDrop; yoshi] with
+      ambientLight = Some ((Color.scale 0.2 blue), Lighten)
       shadows = false
       background = black
   }
 
 let options =
   { defaultOptions with 
-      antiAliasing = false 
+      antiAliasing = true
   }
 
 let run () =
